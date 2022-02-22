57da1fda4934a7979276fc3fc653f94deb9d4fed
Simon LE ROUX <98839389+simnlrx@users.noreply.github.com>
2022-02-22 14:43:43 +0100

    First stage of the project

---
+++ src/fr/loadrunner/LoadRunner.java
@@ -0,0 +1,8 @@
+package fr.loadrunner;
+
+public class LoadRunner {
+
+    public static void main(String[] args) {
+        System.out.println("LoadRunner | Loading ...");
+    }
+}
---
+++ src/fr/loadrunner/games/Board.java
@@ -0,0 +1,7 @@
+package fr.loadrunner.games;
+
+public class Board {
+
+    private String name = "Plateau";
+
+}
