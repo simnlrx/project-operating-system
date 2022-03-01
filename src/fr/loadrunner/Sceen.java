cedc33e4c09ad5469f11230c201f1c4cb960d6ce
xray5 <73293519+xray5@users.noreply.github.com>
2022-03-01 23:02:58 +0100

    ENFIN
    Enfin une bonne noivelle avec une génération aléatoire du terrein qui marche, manque plus que la génération des escaliers (in progress)

--- src/fr/loadrunner/Sceen.java
+++ src/fr/loadrunner/Sceen.java
@@ -10,13 +10,10 @@
 // 11 - représente le joueur en multijoueur
 
 
-
-
 public class Sceen{
     private int height; //Hauteur de l'écran
     private int lenght; //Largeur de l'écran
     private int tab[][]; //scene représentée par une matrice 2*2
-    private int baseStairsYNextPlatform;
 
     //constructeur de Sceen
     public Sceen(int height, int lenght){
@@ -72,69 +69,75 @@ public class Sceen{
 
     //fonction permettant de générée la scene principale du jeu
     public void genSceen(){
-      boolean direction= true;//sens de création de la platforme (true=gauche à droite, false = droite à gauche)
       int baseStairsX = (int)(Math.random()*32)+1;//premier random permetant de trouver la base du premier escalier
-      int baseStairsY = this.height-1;//emplacement
-      for(int i=0;i<4;i++){//boucle pour générer les 4 différents escaliers
-        genPlatform(baseStairsX,baseStairsY,direction);
-        direction = !direction;
-        for(int y=1;y<=4;y++){//création de l'escalier lié à la platforme
-          if(tab[baseStairsY-1][baseStairsYNextPlatform]!=1){
-            tab[baseStairsY-1][baseStairsYNextPlatform] = 3;//la valeur de la case prend 3 pour placerun escalier
-            baseStairsY--;
-          }
+      int baseStairsY = this.height-2;//emplacement
+      genPlatform(baseStairsX,baseStairsY);
+    }
+
+    public void genStairs(int baseStairsY, int baseStairsX){
+      for(int i=1;i<5;i++){
+        if(tab[baseStairsY+i][baseStairsX]!=1){
+          tab[baseStairsY+i][baseStairsX]=3;
         }
       }
     }
 
     //fonction qui va permettre de générée une platforme
     //renvoie selon l'axe x, l'endroit de la prochaine base de l'escalier
-    public void genPlatform(int baseStairsX, int baseStairsY, boolean direction){
-      int nbPlatform = (int)(Math.random()*10)+1;//créé un nombre aléatoire de parties d'une platforme
+    public void genPlatform(int baseStairsX, int baseStairsY){
+      int stage = baseStairsY;//nombres de niveaux restants
+      int stairsBase = 0;//base de l'échelle courrante
+      int nbPlatform;//créé un nombre aléatoire de parties d'une platforme
       int edegeOfPlat;//pointeur qui va pointer l'endroit de la génération d'une platforme
-      int baseStairsYNextPlatform = tab[baseStairsY][baseStairsX];//base de la prochaine platforme initialisée sur la premiere paltaforme
-      if(direction){//création de platformes de gauche à droite
-        edegeOfPlat = baseStairsX;
-          if(tab[baseStairsY][edegeOfPlat-3]==9 || tab[baseStairsY][edegeOfPlat-2]==9 || tab[baseStairsY][edegeOfPlat-1]==9){//Si un bord est sur le chemin de la création de la platforme
-            edegeOfPlat = baseStairsY;//alors il n'y a pas de bord gauche
+      boolean direction = true;
+      do{
+        if(direction){//création de platformes de gauche à droite
+          nbPlatform = (int)(Math.random()*24)+8;
+          stairsBase = (int)(Math.random()*nbPlatform);
+          System.out.println(direction);
+          if(tab[stage][baseStairsX-3]==9 || tab[stage][baseStairsX-2]==9 || tab[stage][baseStairsX-1]==9 || tab[stage][baseStairsX]==9){//Si un bord est sur le chemin de la création de la platforme
+            edegeOfPlat = baseStairsX+3;//alors il n'y a pas de bord gauche
           }else{// si il n'y a pas de bord dans sur le chemin de la généaration des platformes
-            edegeOfPlat = baseStairsX-3;//le bord se situe à gauche de la platforme
+            edegeOfPlat = baseStairsX-2;//le bord se situe à gauche de la platforme
+            while(nbPlatform>0){//tant qu'il y a des platformes de disponible, création d'une nouvelle partie de la platforme
+              if(tab[stage][edegeOfPlat]==9){
+                nbPlatform=0;//Avant de commencer la génératio, si le pointeur pointe ver un bord, on stop la génération
+              }
+              else if(tab[stage][edegeOfPlat]==3){//si la génération de platforme rencontre un escalier, il avance d'un cran
+                edegeOfPlat++;//on passe l'escalier
+              }else if(tab[stage][edegeOfPlat]!=3){//si il n'y a pas d'escalier à l'endroit actuel et pas de bord
+                tab[stage][edegeOfPlat]=2;//la case de la matrice prend la valeur 2 (une platforme)
+                edegeOfPlat++;//on incrémente la base de la platforme poiur la décaler de 1 unitée à gauche
+                nbPlatform--;//on enleve 1 platforme du nombre de platforme à posées
+              }
+            }
           }
-        while(nbPlatform>0){//tant qu'il y a des platformes de disponible, création d'une nouvelle partie de la platforme
-              System.out.println("C'est ok 1");
-          if(tab[edegeOfPlat][baseStairsX]==9){
-            nbPlatform=0;//Avant de commencer la génératio, si le pointeur pointe ver un bord, on stop la génération
+          //genStairs(stage, stairsBase);
+        }else{//création de platformes de droite à gauche
+          System.out.println(direction);
+          nbPlatform = (int)(Math.random()*24)+5;
+            if(tab[stage][baseStairsX+3]==9 || tab[stage][baseStairsX+2]==9 || tab[stage][baseStairsX+1]==9 || tab[stage][baseStairsX]==9){//Si un bord est sur le chemin de la création de la platforme
+              edegeOfPlat = baseStairsX-3;//alors il n'y a pas de bord droit
+            }else{//si il n'y a pas de bord dans sur le chemin de la généaration des platformes
+              edegeOfPlat = baseStairsX+2;//le bord se situe à droite de la platforme
+            }
+          while(nbPlatform!=0){//tant qu'il y a des platformes de disponible, création d'une nouvelle partie de la platforme
+            if(tab[stage][edegeOfPlat]==9){
+              nbPlatform=0;//Avant de commencer la génération, si le pointeur pointe ver un bord, on stop la génération
+            }
+            else if(tab[stage][edegeOfPlat]==3){//si la génération de platforme rencontre un escalier, il avance d'un cran
+              edegeOfPlat--;//si un escalier est déja sur cete case, on passe l'escalier
+            }else if(tab[stage][edegeOfPlat]!=3){//si il n'y a pas d'escalier à l'endroit actuel et pas de bord
+              tab[stage][edegeOfPlat]=2;//la case de la matrice va prendre la valeur 2 (un escalier)
+              edegeOfPlat--;//on décale le bord
+              nbPlatform--;//on enleve 1 platforme du nombre de platforme à posées
+            }
           }
-          if(tab[edegeOfPlat][baseStairsX]==3){//si la génération de platforme rencontre un escalier, il avance d'un cran
-            edegeOfPlat++;//on passe l'escalier
-          }else if(tab[edegeOfPlat][baseStairsX]!=3){//si il n'y a pas d'escalier à l'endroit actuel et pas de bord
-            this.tab[edegeOfPlat][baseStairsX]=2;//la case de la matrice prend la valeur 2 (une platforme)
-            edegeOfPlat++;//on incrémente la base de la platforme poiur la décaler de 1 unitée à gauche
-            nbPlatform--;//on enleve 1 platforme du nombre de platforme à posées
-          }
+          //genStairs(stage, stairsBase);
         }
-      }else{//création de platformes de droite à gauche
-        edegeOfPlat = baseStairsX;
-          if(tab[baseStairsY][edegeOfPlat+3]==9 || tab[baseStairsY][edegeOfPlat+2]==9 || tab[baseStairsY][edegeOfPlat+1]==9){//Si un bord est sur le chemin de la création de la platforme
-            edegeOfPlat = baseStairsY;//alors il n'y a pas de bord droit
-          }else{//si il n'y a pas de bord dans sur le chemin de la généaration des platformes
-          edegeOfPlat = baseStairsX+3;//le bord se situe à droite de la platforme
-          }
-        while(nbPlatform!=0){//tant qu'il y a des platformes de disponible, création d'une nouvelle partie de la platforme
-          if(tab[baseStairsY][edegeOfPlat]==9) nbPlatform=0;//Avant de commencer la génération, si le pointeur pointe ver un bord, on stop la génération
-          if(tab[baseStairsY][edegeOfPlat]==3){//si la génération de platforme rencontre un escalier, il avance d'un cran
-            edegeOfPlat--;//si un escalier est déja sur cete case, on passe l'escalier
-          }else if(tab[baseStairsY][edegeOfPlat]!=3){//si il n'y a pas d'escalier à l'endroit actuel et pas de bord
-            this.tab[baseStairsY][edegeOfPlat]=2;//la case de la matrice va prendre la valeur 2 (un escalier)
-            edegeOfPlat--;//on décale le bord
-            nbPlatform--;//on enleve 1 platforme du nombre de platforme à posées
-          }
-        }
-      }
-      while(tab[baseStairsY][baseStairsYNextPlatform]==1 || tab[baseStairsY][baseStairsYNextPlatform]==0){//tant que la base de la prochaine platforme se situe sur un escalier ou dans le vide
-        baseStairsYNextPlatform = (int)(Math.random()*34)+1;//on tire un nouvel endroit
-      }
-      this.baseStairsYNextPlatform = baseStairsYNextPlatform;
+        direction = !direction;
+        stage = stage-4;
+      }while(stage>=4);
     }
 
 }
--- src/fr/loadrunner/game.java
+++ src/fr/loadrunner/game.java
@@ -2,8 +2,9 @@ public class Game{
   public static void main(String[]arg){
     Player joueur1 = new Player(10, "Bastien");
     Player joueur2 = new Player(10, "Simon");
-    Sceen sceen = new Sceen(17,34);//les valeurs 17 et 36 sont faites pour coller avec les méthodes de création des escaliers =>17-1(pour le bord)= 4 escaliers
-    //sceen.genSceen();//test d'affichage de mla méthode Matrix2Screen
-    sceen.printMatrix();
+    Sceen sceen = new Sceen(18,34);//les valeurs 17 et 36 sont faites pour coller avec les méthodes de création des escaliers =>17-1(pour le bord)= 4 escaliers
+    sceen.matrix2Screen();
+    sceen.genSceen();//test d'affichage de mla méthode Matrix2Screen
+    sceen.matrix2Screen();
   }
 }
