27d0ec34b8c6a86af4f761efb3984e1e466d9553
xray5 <73293519+xray5@users.noreply.github.com>
2022-03-02 12:09:27 +0100

    Edit

--- fr/loadrunner/Game.java
+++ fr/loadrunner/Game.java
@@ -3,10 +3,9 @@ public class Game{
     int optionGame;
     Player joueur1 = new Player(0, "Bastien");
     Player joueur2 = new Player(0, "Simon");
-    Scene scene = new Scene(18,34,joueur1, joueur2);//les valeurs 17 et 36 sont faites pour coller avec les méthodes de création des escaliers =>17-1(pour le bord)= 4 escaliers
-    scene.genSceenLevel1();//test d'affichage de mla méthode Matrix2Screen
+    Scene scene = new Scene(18,34,joueur1);//les valeurs 17 et 36 sont faites pour coller avec les méthodes de création des escaliers =>17-1(pour le bord)= 4 escaliers
+    scene.genSceneLevel1();//test d'affichage de mla méthode Matrix2Screen
     scene.matrix2Screen();
-
     scene.printMatrix();
   }
 }
