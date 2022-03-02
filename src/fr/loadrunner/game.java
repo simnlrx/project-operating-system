a79f0d490b12ba475bb1e29f29d7aa986bb08973
Simon LE ROUX <98839389+simnlrx@users.noreply.github.com>
2022-03-02 11:09:52 +0100

    Delete src/fr/loadrunner directory

--- src/fr/loadrunner/game.java
+++
@@ -1,9 +0,0 @@
-public class Game{
-  public static void main(String[]arg){
-    Player joueur1 = new Player(0, "Bastien");
-    Player joueur2 = new Player(0, "Simon");
-    Sceen sceen = new Sceen(18,34,joueur1, joueur2);//les valeurs 17 et 36 sont faites pour coller avec les méthodes de création des escaliers =>17-1(pour le bord)= 4 escaliers
-    sceen.genSceenLevel1();//test d'affichage de mla méthode Matrix2Screen
-    sceen.matrix2Screen();
-  }
-}
