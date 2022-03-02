898b5dde742da422ece3f581a841ce0c7da86eee
Simon LE ROUX <98839389+simnlrx@users.noreply.github.com>
2022-03-02 11:08:51 +0100

    Edit

---
+++ fr/loadrunner/Game.java
@@ -0,0 +1,11 @@
+public class Game{
+  public static void main(String[]arg){
+    Player joueur1 = new Player(0, "Bastien");
+    Player joueur2 = new Player(0, "Simon");
+    Scene scene = new Scene(18,34,joueur1, joueur2);//les valeurs 17 et 36 sont faites pour coller avec les méthodes de création des escaliers =>17-1(pour le bord)= 4 escaliers
+    scene.genSceenLevel1();//test d'affichage de mla méthode Matrix2Screen
+    scene.matrix2Screen();
+
+    scene.printMatrix();
+  }
+}
