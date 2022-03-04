e09575ab6c6cac68ccfa71bb524117bf387d1e0c
Mellisen <42451222+Mellisen@users.noreply.github.com>
2022-03-04 20:13:49 +0100

    edit

--- fr/loadrunner/LoadRunner.java
+++ fr/loadrunner/LoadRunner.java
@@ -1,13 +1,16 @@
+import game.Player;
+import game.Scene;
+import handler.GameManager;
+
 //
 public class LoadRunner {
 
     public static void main(String[] args) {
         System.out.println("Load Runner | Runnig ...");
-        /*
         Player player1 = new Player(0, "Bastien", 1);
         Scene scene = new Scene(18,34,player1); //les valeurs 17 et 36 sont faites pour coller avec les méthodes de création des escaliers =>17-1(pour le bord)= 4 escaliers
         GameManager game = new GameManager(player1, scene);
         game.start();
-        */
+
     }
 }
--- fr/loadrunner/game/Scene.java
+++ fr/loadrunner/game/Scene.java
@@ -16,50 +16,50 @@ package game;
 
 public class Scene {
     private int height; //Hauteur de l'écran
-    private int lenght; //Largeur de l'écran
-    private int tab[][]; //scene représentée par une matrice 2*2
+    private int length; //Largeur de l'écran
+    private int board[][]; //scene représentée par une matrice 2*2
     private int baseStairsY;//pointeur selon l'axe Y
     private int baseStairsX;//pointeur selon l'axe X
     private Player player1;//déclaration du joueur1
     private Player player2;//déclaration du joueur2
 
-    //constructeur de game.Scene
-    public Scene(int height, int lenght, Player player1) {
+    //constructeur de Scene
+    public Scene(int height, int length, Player player1) {
         this.player1 = player1;
         this.player2 = new Player(0, "",2);
         this.height = height;
-        this.lenght = lenght;
-        this.tab = new int[height][lenght];
+        this.length = length;
+        this.board = new int[height][length];
         //initialisation d'une Scene remplies préalablement de 0
         for (int i = 0; i < this.height; i++) {
-            for (int y = 0; y < this.lenght; y++) {
+            for (int y = 0; y < this.length; y++) {
                 //déclarations des bordures du terrain
                 if (i == 0 || i == (this.height - 1)) {
-                    tab[i][y] = 1;
-                } else if (y == 0 || y == (this.lenght - 1)) {
-                    tab[i][y] = 9;
+                    board[i][y] = 1;
+                } else if (y == 0 || y == (this.length - 1)) {
+                    board[i][y] = 9;
                 } else {
-                    tab[i][y] = 0;
+                    board[i][y] = 0;
                 }
             }
         }
     }
 
-    public Scene(int height, int lenght, Player player1, Player player2) {
+    public Scene(int height, int length, Player player1, Player player2) {
         this.player1 = player1;
         this.player2 = player2;
         this.height = height;
-        this.lenght = lenght;
-        this.tab = new int[height][lenght];
+        this.length = length;
+        this.board = new int[height][length];
         for (int i = 0; i < this.height; i++) {
-            for (int y = 0; y < this.lenght; y++) {
+            for (int y = 0; y < this.length; y++) {
                 //déclarations des bordures du terrain
                 if (i == 0 || i == (this.height - 1)) {
-                    tab[i][y] = 1;
-                } else if (y == 0 || y == (this.lenght - 1)) {
-                    tab[i][y] = 9;
+                    board[i][y] = 1;
+                } else if (y == 0 || y == (this.length - 1)) {
+                    board[i][y] = 9;
                 } else {
-                    tab[i][y] = 0;
+                    board[i][y] = 0;
                 }
             }
         }
@@ -68,8 +68,8 @@ public class Scene {
     //fonction permettant d'afficher la matrice de l'écran
     public void printMatrix() {
         for (int i = 0; i < (this.height); i++) {
-            for (int y = 0; y < (this.lenght); y++) {
-                System.out.print(tab[i][y] + " ");
+            for (int y = 0; y < (this.length); y++) {
+                System.out.print(board[i][y] + " ");
             }
             System.out.print("\n");
         }
@@ -79,13 +79,13 @@ public class Scene {
     public void matrix2Screen() {
         int value;
         for (int i = 0; i < (this.height); i++) {
-            for (int y = 0; y < (this.lenght); y++) {
-                value = tab[i][y];
+            for (int y = 0; y < (this.length); y++) {
+                value = board[i][y];
                 switch (value) {
                     case 0: {System.out.print("  ");break;}//espace vide
                     case 1: {System.out.print("–-");break;}//bord horizontal
-                    case 2: {System.out.print("==");break;}//platforme
-                    case 3: {System.out.print("##");break;}//échelle
+                    case 2: {System.out.print("▓▓");break;}//platforme
+                    case 3: {System.out.print("│┤");break;}//échelle
                     case 4: {System.out.print("4 ");break;}//simulation d'un ennemi en attente d'un symbole
                     case 5: {System.out.print("☼ ");break;}//simulation d'un objet
                     case 6: {System.out.print("S ");break;}//simulation du spawn
@@ -108,38 +108,38 @@ public class Scene {
         int basePlat = baseStairsY;
         int baseStair = baseStairsY;
         for (int i = 1; i < 18; i++) {//génération d'une premier platforme
-            tab[basePlat][i] = 2;
+            board[basePlat][i] = 2;
         }
         basePlat -= 4;
         for (int i = 16; i < 30; i++) {//génération d'une deuxieme platforme
-            tab[basePlat][i] = 2;
+            board[basePlat][i] = 2;
         }
         basePlat -= 4;
         for (int i = 2; i < 26; i++) {//génération d'une troisieme platforme
-            tab[basePlat][i] = 2;
+            board[basePlat][i] = 2;
         }
         basePlat -= 4;
         for (int i = 5; i < 30; i++) {//génération d'une quatrieme platforme
-            tab[basePlat][i] = 2;
+            board[basePlat][i] = 2;
         }
         for (int i = 1; i < 5; i++) {//generation premier esclaier
-            tab[baseStair - i][16] = 3;
+            board[baseStair - i][16] = 3;
         }
         baseStair -= 4;
         for (int i = 1; i < 5; i++) {//generation deuxieme esclaier
-            tab[baseStair - i][18] = 3;
+            board[baseStair - i][18] = 3;
         }
         baseStair -= 4;
         for (int i = 1; i < 5; i++) {//generation troisieme esclaier
-            tab[baseStair - i][10] = 3;
+            board[baseStair - i][10] = 3;
         }
         baseStair -= 4;
         for (int i = 1; i < 5; i++) {//generation quatrieme esclaier
-            if (tab[baseStair - i][22] != 1) {
-                tab[baseStair - i][22] = 3;
+            if (board[baseStair - i][22] != 1) {
+                board[baseStair - i][22] = 3;
             }
         }
-        tab[baseStairsY - 1][2] = 6;
+        board[baseStairsY - 1][2] = 6;
     }
 
     //fonction permettant de générée la scene principale du jeu
@@ -151,8 +151,8 @@ public class Scene {
 
     public void genStairs(int baseStairsY, int baseStairsX) {//fonction test pour la génération d'escaliers
         for (int i = 1; i < 5; i++) {
-            if (tab[baseStairsY + i][baseStairsX] != 1) {
-                tab[baseStairsY + i][baseStairsX] = 3;
+            if (board[baseStairsY + i][baseStairsX] != 1) {
+                board[baseStairsY + i][baseStairsX] = 3;
             }
         }
     }
@@ -170,17 +170,17 @@ public class Scene {
                 nbPlatform = (int) (Math.random() * 24) + 8;
                 stairsBase = (int) (Math.random() * nbPlatform);
                 System.out.println(direction);
-                if (tab[stage][baseStairsX - 3] == 9 || tab[stage][baseStairsX - 2] == 9 || tab[stage][baseStairsX - 1] == 9 || tab[stage][baseStairsX] == 9) {//Si un bord est sur le chemin de la création de la platforme
+                if (board[stage][baseStairsX - 3] == 9 || board[stage][baseStairsX - 2] == 9 || board[stage][baseStairsX - 1] == 9 || board[stage][baseStairsX] == 9) {//Si un bord est sur le chemin de la création de la platforme
                     edegeOfPlat = baseStairsX + 3;//alors il n'y a pas de bord gauche
                 } else {// si il n'y a pas de bord dans sur le chemin de la généaration des platformes
                     edegeOfPlat = baseStairsX - 2;//le bord se situe à gauche de la platforme
                     while (nbPlatform > 0) {//tant qu'il y a des platformes de disponible, création d'une nouvelle partie de la platforme
-                        if (tab[stage][edegeOfPlat] == 9) {
+                        if (board[stage][edegeOfPlat] == 9) {
                             nbPlatform = 0;//Avant de commencer la génératio, si le pointeur pointe ver un bord, on stop la génération
-                        } else if (tab[stage][edegeOfPlat] == 3) {//si la génération de platforme rencontre un escalier, il avance d'un cran
+                        } else if (board[stage][edegeOfPlat] == 3) {//si la génération de platforme rencontre un escalier, il avance d'un cran
                             edegeOfPlat++;//on passe l'escalier
-                        } else if (tab[stage][edegeOfPlat] != 3) {//si il n'y a pas d'escalier à l'endroit actuel et pas de bord
-                            tab[stage][edegeOfPlat] = 2;//la case de la matrice prend la valeur 2 (une platforme)
+                        } else if (board[stage][edegeOfPlat] != 3) {//si il n'y a pas d'escalier à l'endroit actuel et pas de bord
+                            board[stage][edegeOfPlat] = 2;//la case de la matrice prend la valeur 2 (une platforme)
                             edegeOfPlat++;//on incrémente la base de la platforme poiur la décaler de 1 unitée à gauche
                             nbPlatform--;//on enleve 1 platforme du nombre de platforme à posées
                         }
@@ -190,18 +190,18 @@ public class Scene {
             } else {//création de platformes de droite à gauche
                 System.out.println(direction);
                 nbPlatform = (int) (Math.random() * 24) + 8;
-                if (tab[stage][baseStairsX + 3] == 9 || tab[stage][baseStairsX + 2] == 9 || tab[stage][baseStairsX + 1] == 9 || tab[stage][baseStairsX] == 9) {//Si un bord est sur le chemin de la création de la platforme
+                if (board[stage][baseStairsX + 3] == 9 || board[stage][baseStairsX + 2] == 9 || board[stage][baseStairsX + 1] == 9 || board[stage][baseStairsX] == 9) {//Si un bord est sur le chemin de la création de la platforme
                     edegeOfPlat = baseStairsX - 3;//alors il n'y a pas de bord droit
                 } else {//si il n'y a pas de bord dans sur le chemin de la généaration des platformes
                     edegeOfPlat = baseStairsX + 2;//le bord se situe à droite de la platforme
                 }
                 while (nbPlatform != 0) {//tant qu'il y a des platformes de disponible, création d'une nouvelle partie de la platforme
-                    if (tab[stage][edegeOfPlat] == 9) {
+                    if (board[stage][edegeOfPlat] == 9) {
                         nbPlatform = 0;//Avant de commencer la génération, si le pointeur pointe ver un bord, on stop la génération
-                    } else if (tab[stage][edegeOfPlat] == 3) {//si la génération de platforme rencontre un escalier, il avance d'un cran
+                    } else if (board[stage][edegeOfPlat] == 3) {//si la génération de platforme rencontre un escalier, il avance d'un cran
                         edegeOfPlat--;//si un escalier est déja sur cete case, on passe l'escalier
-                    } else if (tab[stage][edegeOfPlat] != 3) {//si il n'y a pas d'escalier à l'endroit actuel et pas de bord
-                        tab[stage][edegeOfPlat] = 2;//la case de la matrice va prendre la valeur 2 (un escalier)
+                    } else if (board[stage][edegeOfPlat] != 3) {//si il n'y a pas d'escalier à l'endroit actuel et pas de bord
+                        board[stage][edegeOfPlat] = 2;//la case de la matrice va prendre la valeur 2 (un escalier)
                         edegeOfPlat--;//on décale le bord
                         nbPlatform--;//on enleve 1 platforme du nombre de platforme à posées
                     }
@@ -215,20 +215,20 @@ public class Scene {
 
     public void setPositionPlayer(Player player, int x, int y){
       if(player.getNumber()==1){
-        tab[y][x] = 1;
+        board[y][x] = 1;
         player.setPosition(x, y);
       }
       else if(player.getNumber()==2){
-        tab[y][x] = 2;
+        board[y][x] = 2;
         player.setPosition(x, y);
       }
     }
 
     public int getValueScene(int x,int y){
-      return tab[y][x];
+      return board[y][x];
     }
 
     public void setValueScene(int x,int y, int value){
-      tab[y][x] = value;
+      board[y][x] = value;
     }
 }
