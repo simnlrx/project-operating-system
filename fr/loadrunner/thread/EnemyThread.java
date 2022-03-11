6e80a9d3ca906d0065f46a371fb5cd7d79cf72b0
xray5 <73293519+xray5@users.noreply.github.com>
2022-03-11 10:00:34 +0100

    Edit Moove class Enemy

--- fr/loadrunner/thread/EnemyThread.java
+++ fr/loadrunner/thread/EnemyThread.java
@@ -1,19 +1,39 @@
 package game;
 
 public class Enemy extends Thread{
-  private int posX;
-  private int posY;
-  private Scene scene;
-  private boolean direction;
+  private int posX;//position en x de l'ennemi
+  private int posY;//position en y de l'ennemi
+  private Scene scene;//scene de l'ennemi
+  private boolean sens;//sens du mouvement de l'ennemi
 
-  public Enemy(int posX, int posY, Scene scene){
+  public Enemy(Scene scene){
     this.posX = posX;
     this.posY = posY;
     this.scene = scene;
+    this.sens = true;
+  }
+
+  public void chooseDirection(){
+    if(scene.getValueScene(posX-1, posY+1)==0 || scene.getValueScene(posX-1, posY+1)==1){
+      //si l'ennemi se deplace de droite à gauche et qu'il rencontre le bord de la platforme ou le bord de l'écran
+      this.sens = false;//alors il change de sens de déplacement
+    }else if(scene.getValueScene(posX+1, posY+1)==0 || scene.getValueScene(posX-1, posY+1)==1)
+      //si l'ennemi se deplace de gauche à droite et qu'il rencontre le bord de la platforme ou le bord de l'écran
+      this.sens = true;//alors il change de sens de déplacement
   }
 
   @Override
   public void run(){
-
+    chooseDirection();
+    if(sens){//si le sens est à true
+      scene.setValueScene(posX-1, posY+1, 4);//on déplace le joueur d'une case à gauche
+      scene.setValueScene(posX, posY, 0);//la position précédente de l'ennemi repasse à un espace vide
+      this.posX--;//on décrémente la position en x du joueur de 1
+    }
+    else if(!sens){//si le sens est à false
+      scene.setValueScene(posX+1, posY+1, 4);//on déplace le joueur d'une case à droite
+      scene.setValueScene(posX, posY, 0);//la position précédente de l'ennemi repasse à un espace vide
+      this.posX++;//on incrémente la position en x du joueur de 1
+    }
   }
 }
