bbaf436fbf0ae6bfd7ce601b85eecc873fb40ffe
xray5 <bast.gouneaud@gmail.com>
2022-04-11 20:23:12 +0200

    Update KeySelection.java

--- LodeRunner/src/LodeRunner/game/KeySelection.java
+++ LodeRunner/src/LodeRunner/game/KeySelection.java
@@ -39,7 +39,7 @@ public class KeySelection {
                         scene.setPositionPlayer(player, x, y - 1);
                     }
                 case 'q':
-                    if (left == 0 || left == 3 || left == 14 || left == 10 || left == 4 || left == 13 || left == 16 || left == 5 || left == 11) {
+                    if (left == 0 || left == 3 || left == 14 || left == 10 || left == 4 || left == 13 || left == 16 || left == 5) {
                         if (scene.getValuePosition(x - 1, y + 1) == 2 || scene.getValuePosition(x - 1, y + 1) == 3 || scene.getValuePosition(x - 1, y + 1) == 4) {
                             scene.setValuePosition(x, y, 0);
                             scene.setPositionPlayer(player, x - 1, y);
